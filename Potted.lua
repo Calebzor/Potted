9a76588fee240cc6d21648071852d88457305477
Caleb <calebzor@gmail.com>
2014-09-24 21:15:13 +0200

    fix expert insight boost spellId

--- Potted.lua
+++ Potted.lua
@@ -13,7 +13,7 @@
 		localization
 ]]--
 
-local sVersion = "9.0.1.4"
+local sVersion = "9.0.1.5"
 
 require "GameLib"
 require "GroupLib"
@@ -107,7 +107,7 @@ function addon:OnInitialize()
 		[35078] = true, -- Liquid Focus -- Reactive Strikethrough Boost
 		[36588] = true, -- Expert Moxie Boost - Moxie Boost
 		[36573] = true, -- Expert Finess Boost - Finess Boost
-		[26594] = true, -- Expert Insight Boost - Insight Boost
+		[36594] = true, -- Expert Insight Boost - Insight Boost
 		[35028] = true, -- Expert Brutality Boost - Brutality Boost
 		[38157] = true, -- Expert Grid Boost - Grit boost
 		[36579] = true, -- Expert Tech Boost - Tech boost
