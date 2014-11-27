94e2aa7162b1050eb39762ed4c11d44f841e82bd
Caleb <calebzor@gmail.com>
2014-11-27 20:40:27 +0100

    fix old life steal spellId

--- Potted.lua
+++ Potted.lua
@@ -13,7 +13,7 @@
 		localization
 ]]--
 
-local sVersion = "9.0.1.8"
+local sVersion = "9.0.1.10"
 
 require "GameLib"
 require "GroupLib"
@@ -146,7 +146,7 @@ function addon:OnInitialize()
 	self.tFieldTechtIds = {
 		--[32821] = true, -- bolster
 		[35213] = true, -- Liquid Confidence - Siphon
-		[35147] = true, -- Life Drain
+		[35145] = true, -- Life Drain
 		[35164] = true, -- Bioreactive Acid Membrane - Cleave
 
 		[35166] = true, -- Echo Impact Synchronizer - Cleave
